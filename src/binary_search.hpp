3d172045569041dc8a1b158221883fe4db3f9d89
amartinsmg <andremindurimg@gmail.com>
2022-11-17 20:59:25 -0300

    Add binary_search.hpp

---
+++ src/binary_search.hpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cassert>
+
+template <typename T>
+int binary_search(T item, T *values, int length)
+{
+  int low = 0,
+      high = length - 1,
+      mid = high / 2,
+      result = -1;
+  assert(length > 0);
+  while (result == -1 && low <= high)
+  {
+    if (values[mid] == item)
+      result = mid;
+    else if (values[mid] > item)
+      high = mid - 1;
+    else
+      low = mid + 1;
+    mid = (low + high) / 2;
+  }
+
+  return result;
+}
