e7e6ca93b4fb8d649540610a9d4ca3187131236c
andre <andremindurimg@gmail.com>
2023-06-29 19:25:12 -0300

    Update exponential_search.hpp

--- src/exponential_search.hpp
+++ src/exponential_search.hpp
@@ -4,17 +4,13 @@ template <typename T>
 int exponential_search(T item, T *values, int length)
 {
   int low = 0, high, mid, result = -1;
-
   assert(length > 0);
-
   for (high = 1; high < length && values[high - 1] < item; high *= 2)
     low = high - 1;
-
   if (high > length)
     high = length - 1;
   else
     high--;
-
   while (result == -1 && low <= high)
   {
     mid = (low + high) / 2;
@@ -25,6 +21,5 @@ int exponential_search(T item, T *values, int length)
     else
       low = mid + 1;
   }
-
   return result;
 }
