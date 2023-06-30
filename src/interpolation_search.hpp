721ce5a04f80443b909abd5af6d83c895e9bf9ba
andre <andremindurimg@gmail.com>
2023-06-30 17:06:44 -0300

    Update interpolation_search.hpp

--- src/interpolation_search.hpp
+++ src/interpolation_search.hpp
@@ -1,22 +1,22 @@
 #include <cassert>
 
 template <typename T>
-int interpolationSearch(T target, T *values, int length)
+int interpolationSearch(T target, T *arr, int length)
 {
   int low = 0,
       high = length - 1,
       pos,
       result = -1;
   assert(length > 0);
-  while (low <= high && values[low] <= target && values[high] >= target)
+  while (low <= high && arr[low] <= target && arr[high] >= target)
   {
-    pos = values[low] < values[high] ? low + (high - low) * (int)((target - values[low]) / (values[high] - values[low])) : low;
-    if (values[pos] == target)
+    pos = arr[low] < arr[high] ? low + (high - low) * (int)((target - arr[low]) / (arr[high] - arr[low])) : low;
+    if (arr[pos] == target)
     {
       result = pos;
       break;
     }
-    else if (values[pos] > target)
+    else if (arr[pos] > target)
       high = pos - 1;
     else
       low = pos + 1;
