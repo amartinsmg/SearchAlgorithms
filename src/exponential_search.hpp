e71e8230ec3b4b5e54da5744e9b542cca577b148
andre <andremindurimg@gmail.com>
2023-06-30 17:06:32 -0300

    Update exponential_search.hpp

--- src/exponential_search.hpp
+++ src/exponential_search.hpp
@@ -2,24 +2,24 @@
 #include <cassert>
 
 template <typename T>
-int exponentialSearch(T target, T *values, int length)
+int exponentialSearch(T target, T *arr, int length)
 {
   int high, i, mid,
       low = 0,
       result = -1;
   assert(length > 0);
-  for (i = 1; i < length && values[i - 1] < target; i *= 2)
+  for (i = 1; i < length && arr[i - 1] < target; i *= 2)
     low = i - 1;
   high = std::min(i, length) - 1;
   while (low <= high)
   {
     mid = (low + high) / 2;
-    if (values[mid] == target)
+    if (arr[mid] == target)
     {
       result = mid;
       break;
     }
-    else if (values[mid] > target)
+    else if (arr[mid] > target)
       high = mid - 1;
     else
       low = mid + 1;
