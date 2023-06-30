c51ed2695ed420596dfe077c54a963d1dbea6733
andre <andremindurimg@gmail.com>
2023-06-30 16:58:29 -0300

    Update linear_search.hpp

--- src/linear_search.hpp
+++ src/linear_search.hpp
@@ -1,12 +1,12 @@
 #include <cassert>
 
 template <typename T>
-int linearSearch(T item, T *values, int length)
+int linearSearch(T target, T *values, int length)
 {
   int i, result = -1;
   assert(length > 0);
   for (i = 0; i < length; i++)
-    if (values[i] == item)
+    if (values[i] == target)
     {
       result = i;
       break;
