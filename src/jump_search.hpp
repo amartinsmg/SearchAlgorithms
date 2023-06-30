84efd1c655bd606c45fb1e7f656a174c6695e0a0
andre <andremindurimg@gmail.com>
2023-06-30 17:06:56 -0300

    Update jump_search.hpp

--- src/jump_search.hpp
+++ src/jump_search.hpp
@@ -3,18 +3,18 @@
 #include <cmath>
 
 template <typename T>
-int jumpSearch(T target, T *values, int length)
+int jumpSearch(T target, T *arr, int length)
 {
   int i, high, step,
       low = 0,
       result = -1;
   assert(length > 0);
   step = (int)round(sqrt((double)length));
-  for (i = step; i < length && values[i] < target; i += step)
+  for (i = step; i < length && arr[i] < target; i += step)
     low = i;
   high = std::min(length, i);
   for (i = low; i < high; i++)
-    if (values[i] == target)
+    if (arr[i] == target)
     {
       result = i;
       break;
