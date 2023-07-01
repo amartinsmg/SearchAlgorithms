3d4efaf1b962b5e6b3f19be032ca0087704fd4eb
andre <andremindurimg@gmail.com>
2023-07-01 18:48:23 -0300

    Update interpolation_search.hpp

--- src/interpolation_search.hpp
+++ src/interpolation_search.hpp
@@ -1,5 +1,14 @@
 #include <cassert>
 
+/**
+  @brief Performs a interpolation search on a sorted array to find a target value.
+  @tparam T The type of the elements in the array.
+  @param target The target value to search for.
+  @param arr The sorted array to search in.
+  @param length The length of the array.
+  @return The index of the target value in the array if found, -1 otherwise.
+*/
+
 template <typename T>
 int interpolationSearch(T target, T *arr, int length)
 {
