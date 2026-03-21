1be8ede510c4e359598817b39f2f97110526904a
amartinsmg <andremindurimg@gmail.com>
2026-03-21 00:51:01 -0300

    refactor: add aggregate header for search algorithms

---
+++ include/sort.hpp
@@ -0,0 +1,13 @@
+#ifndef SORT_HPP
+#define SORT_HPP
+
+#include "binary_search.hpp"
+#include "exponential_search.hpp"
+#include "fibonacci_search.hpp"
+#include "interpolation_search.hpp"
+#include "jump_search.hpp"
+#include "linear_search.hpp"
+#include "meta_binary_search.hpp"
+#include "sentinel_search.hpp"
+
+#endif /* SORT_HPP */
