8c51d75e84eccc742e6ddf2c35b143ee3626256c
amartinsmg <andremindurimg@gmail.com>
2026-03-22 15:32:44 -0300

    refactor(search): update sentinelSearch signature
    - Reordered parameters for consistency
    - Updated doxygen comments to match signature
    - Simplified empty array check condition

--- include/sentinel_search.hpp
+++ include/sentinel_search.hpp
@@ -3,35 +3,31 @@
 
 #include <vector>
 
-namespace Search
-{
-  /**
-   * @brief Performs a sentinel linear search on a sorted vector to find a target value.
-   * @tparam T The type of the elements in the vector.
-   * @param target The target value to search for.
-   * @param arr The sorted vector to search in.
-   * @return The index of the target value in the vector if found, -1 otherwise.
-   */
+namespace Search {
+/**
+ * @brief Performs a sentinel linear search on a sorted vector to find a target
+ * value.
+ * @tparam T The type of the elements in the vector.
+ * @param arr The sorted vector to search in.
+ * @param target The target value to search for.
+ * @return The index of the target value in the vector if found, -1 otherwise.
+ */
 
-  template <typename T>
-  static int sentinelSearch(T target, std::vector<T> arr)
-  {
-    if (arr.size() <= 0)
-      return -1;
-    int i,
-        length = arr.size(),
-        result = -1;
-    T last;
-    last = arr[length - 1];
-    arr[length - 1] = target;
-    i = 0;
-    while (arr[i] != target)
-      i++;
-    if (i < length - 1 || last == target)
-      result = i;
-    arr[length - 1] = last;
-    return result;
-  }
+template <typename T> static int sentinelSearch(std::vector<T> arr, T target) {
+  if (arr.size() == 0)
+    return -1;
+  int i, length = arr.size(), result = -1;
+  T last;
+  last = arr[length - 1];
+  arr[length - 1] = target;
+  i = 0;
+  while (arr[i] != target)
+    i++;
+  if (i < length - 1 || last == target)
+    result = i;
+  arr[length - 1] = last;
+  return result;
+}
 
 } // namespace Search
 
