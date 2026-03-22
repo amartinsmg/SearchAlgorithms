d66040d2bd58ec3bc9b9ba0812ca41e038c68a57
amartinsmg <andremindurimg@gmail.com>
2026-03-22 15:31:31 -0300

    refactor(linear-search): Improve implementation
    - Cache array size in local variable
    - Remove redundant empty array check
    - Swap parameters for consistency
    - Adjust namespace brace formatting

--- include/linear_search.hpp
+++ include/linear_search.hpp
@@ -3,29 +3,24 @@
 
 #include <vector>
 
-namespace Search
-{
-  /**
-   * @brief Performs a linear search on a sorted vector to find a target value.
-   * @tparam T The type of the elements in the vector.
-   * @param target The target value to search for.
-   * @param arr The sorted vector to search in.
-   * @return The index of the target value in the vector if found, -1 otherwise.
-   */
-  template <typename T>
-  static int linearSearch(T target, std::vector<T> arr)
-  {
-    if (arr.size() <= 0)
-      return -1;
-    int i, result = -1;
-    for (i = 0; i < arr.size(); i++)
-      if (arr[i] == target)
-      {
-        result = i;
-        break;
-      }
-    return result;
-  }
+namespace Search {
+/**
+ * @brief Performs a linear search on a sorted vector to find a target value.
+ * @tparam T The type of the elements in the vector.
+ * @param target The target value to search for.
+ * @param arr The sorted vector to search in.
+ * @return The index of the target value in the vector if found, -1 otherwise.
+ */
+
+template <typename T> static int linearSearch(std::vector<T> arr, T target) {
+  int i, length = arr.size(), result = -1;
+  for (i = 0; i < length; i++)
+    if (arr[i] == target) {
+      result = i;
+      break;
+    }
+  return result;
+}
 
 } // namespace Search
 
