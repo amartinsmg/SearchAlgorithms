1d125dfc388a51a76ba50d80414fa4160cc0f5f2
amartinsmg <andremindurimg@gmail.com>
2026-03-22 15:28:13 -0300

    refactor(jump_search): update function signature
    - Reorder parameters for consistency
    - Adjust empty array check condition
    - Apply minor formatting changes

--- include/jump_search.hpp
+++ include/jump_search.hpp
@@ -2,40 +2,33 @@
 #define JUMP_SEARCH_HPP
 
 #include <algorithm>
-#include <vector>
 #include <cmath>
+#include <vector>
 
-namespace Search
-{
-  /**
-   * @brief Performs a jump search on a sorted vector to find a target value.
-   * @tparam T The type of the elements in the vector.
-   * @param target The target value to search for.
-   * @param arr The sorted vector to search in.
-   * @return The index of the target value in the vector if found, -1 otherwise.
-   */
+namespace Search {
+/**
+ * @brief Performs a jump search on a sorted vector to find a target value.
+ * @tparam T The type of the elements in the vector.
+ * @param target The target value to search for.
+ * @param arr The sorted vector to search in.
+ * @return The index of the target value in the vector if found, -1 otherwise.
+ */
 
-  template <typename T>
-  static int jumpSearch(T target, std::vector<T> arr)
-  {
-    if (arr.size() <= 0)
-      return -1;
-    int i, high, step,
-        length = arr.size(),
-        low = 0,
-        result = -1;
-    step = (int)round(sqrt((double)length));
-    for (i = step; i < length && arr[i] < target; i += step)
-      low = i;
-    high = std::min(length, i);
-    for (i = low; i < high; i++)
-      if (arr[i] == target)
-      {
-        result = i;
-        break;
-      }
-    return result;
-  }
+template <typename T> static int jumpSearch(std::vector<T> arr, T target) {
+  if (arr.size() == 0)
+    return -1;
+  int i, high, step, length = arr.size(), low = 0, result = -1;
+  step = (int)round(sqrt((double)length));
+  for (i = step; i < length && arr[i] < target; i += step)
+    low = i;
+  high = std::min(length, i);
+  for (i = low; i < high; i++)
+    if (arr[i] == target) {
+      result = i;
+      break;
+    }
+  return result;
+}
 
 } // namespace Search
 
