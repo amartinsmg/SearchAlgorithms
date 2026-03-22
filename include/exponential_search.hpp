8fb80a5ef3aa3b8d9015a55ca9891e513c827bcd
amartinsmg <andremindurimg@gmail.com>
2026-03-22 15:23:46 -0300

    refactor(search): update exponentialSearch function
    - Swap target and array parameters
    - Remove redundant empty array check
    - Adjust code formatting and indentation

--- include/exponential_search.hpp
+++ include/exponential_search.hpp
@@ -4,43 +4,33 @@
 #include <algorithm>
 #include <vector>
 
-namespace Search
-{
-  /**
-   * @brief Performs a exponential on a sorted vector to find a target value.
-   * @tparam T The type of the elements in the vector.
-   * @param target The target value to search for.
-   * @param arr The sorted vector to search in.
-   * @return The index of the target value in the vector if found, -1 otherwise.
-   */
+namespace Search {
+/**
+ * @brief Performs a exponential on a sorted vector to find a target value.
+ * @tparam T The type of the elements in the vector.
+ * @param target The target value to search for.
+ * @param arr The sorted vector to search in.
+ * @return The index of the target value in the vector if found, -1 otherwise.
+ */
 
-  template <typename T>
-  static int exponentialSearch(T target, std::vector<T> arr)
-  {
-    if (arr.size() <= 0)
-      return -1;
-    int high, i, mid,
-        length = arr.size(),
-        low = 0,
-        result = -1;
-    for (i = 1; i < length && arr[i - 1] < target; i *= 2)
-      low = i - 1;
-    high = std::min(i, length) - 1;
-    while (low <= high)
-    {
-      mid = (low + high) / 2;
-      if (arr[mid] == target)
-      {
-        result = mid;
-        break;
-      }
-      else if (arr[mid] > target)
-        high = mid - 1;
-      else
-        low = mid + 1;
-    }
-    return result;
+template <typename T>
+static int exponentialSearch(std::vector<T> arr, T target) {
+  int high, i, mid, length = arr.size(), low = 0, result = -1;
+  for (i = 1; i < length && arr[i - 1] < target; i *= 2)
+    low = i - 1;
+  high = std::min(i, length) - 1;
+  while (low <= high) {
+    mid = (low + high) / 2;
+    if (arr[mid] == target) {
+      result = mid;
+      break;
+    } else if (arr[mid] > target)
+      high = mid - 1;
+    else
+      low = mid + 1;
   }
+  return result;
+}
 
 } // namespace Search
 
