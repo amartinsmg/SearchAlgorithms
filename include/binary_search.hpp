4c6b770b4cbdbd416f4d795211eecfeeefb2cf46
amartinsmg <andremindurimg@gmail.com>
2026-03-22 15:23:19 -0300

    refactor(search): update binarySearch function
    - Swap target and array parameters
    - Remove redundant empty array check
    - Adjust code formatting and indentation

--- include/binary_search.hpp
+++ include/binary_search.hpp
@@ -3,40 +3,29 @@
 
 #include <vector>
 
-namespace Search
-{
-  /**
-   * @brief Performs a binary search on a sorted vector to find a target value.
-   * @tparam T The type of the elements in the vector.
-   * @param target The target value to search for.
-   * @param arr The sorted vector to search in.
-   * @return The index of the target value in the vector if found, -1 otherwise.
-   */
+namespace Search {
+/**
+ * @brief Performs a binary search on a sorted vector to find a target value.
+ * @tparam T The type of the elements in the vector.
+ * @param target The target value to search for.
+ * @param arr The sorted vector to search in.
+ * @return The index of the target value in the vector if found, -1 otherwise.
+ */
 
-  template <typename T>
-  static int binarySearch(T target, std::vector<T> arr)
-  {
-    if (arr.size() <= 0)
-      return -1;
-    int low = 0,
-        high = arr.size() - 1,
-        mid = high / 2,
-        result = -1;
-    while (low <= high)
-    {
-      if (arr[mid] == target)
-      {
-        result = mid;
-        break;
-      }
-      else if (arr[mid] > target)
-        high = mid - 1;
-      else
-        low = mid + 1;
-      mid = (low + high) / 2;
-    }
-    return result;
+template <typename T> static int binarySearch(std::vector<T> arr, T target) {
+  int low = 0, high = arr.size() - 1, mid = high / 2, result = -1;
+  while (low <= high) {
+    if (arr[mid] == target) {
+      result = mid;
+      break;
+    } else if (arr[mid] > target)
+      high = mid - 1;
+    else
+      low = mid + 1;
+    mid = (low + high) / 2;
   }
+  return result;
+}
 
 } // namespace Search
 
