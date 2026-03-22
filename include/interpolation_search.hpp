4be7840095e3b969d99b2e67d313f13e1a66bca6
amartinsmg <andremindurimg@gmail.com>
2026-03-22 15:24:02 -0300

    refactor(search): update interpolationSearch function
    - Swap target and array parameters
    - Remove redundant empty array check
    - Adjust code formatting and indentation

--- include/interpolation_search.hpp
+++ include/interpolation_search.hpp
@@ -3,42 +3,34 @@
 
 #include <vector>
 
-namespace Search
-{
-  /**
-   * @brief Performs a interpolation search on a sorted vector to find a target value.
-   * @tparam T The type of the elements in the vector.
-   * @param target The target value to search for.
-   * @param arr The sorted vector to search in.
-   * @return The index of the target value in the vector if found, -1 otherwise.
-   */
+namespace Search {
+/**
+ * @brief Performs a interpolation search on a sorted vector to find a target
+ * value.
+ * @tparam T The type of the elements in the vector.
+ * @param target The target value to search for.
+ * @param arr The sorted vector to search in.
+ * @return The index of the target value in the vector if found, -1 otherwise.
+ */
 
-  template <typename T>
-  static int interpolationSearch(T target, std::vector<T> arr)
-  {
-    if (arr.size() <= 0)
-      return -1;
-    int low = 0,
-        high = arr.size() - 1,
-        pos,
-        result = -1;
-    while (low <= high && arr[low] <= target && arr[high] >= target)
-    {
-      pos = arr[low] < arr[high]
-                ? low + (high - low) * (int)((target - arr[low]) / (arr[high] - arr[low]))
-                : low;
-      if (arr[pos] == target)
-      {
-        result = pos;
-        break;
-      }
-      else if (arr[pos] > target)
-        high = pos - 1;
-      else
-        low = pos + 1;
-    }
-    return result;
+template <typename T>
+static int interpolationSearch(std::vector<T> arr, T target) {
+  int low = 0, high = arr.size() - 1, pos, result = -1;
+  while (low <= high && arr[low] <= target && arr[high] >= target) {
+    pos = arr[low] < arr[high]
+              ? low + (high - low) *
+                          (int)((target - arr[low]) / (arr[high] - arr[low]))
+              : low;
+    if (arr[pos] == target) {
+      result = pos;
+      break;
+    } else if (arr[pos] > target)
+      high = pos - 1;
+    else
+      low = pos + 1;
   }
+  return result;
+}
 
 } // namespace Search
 
